f70bee11afaeb279b6d57f6a352437fce39814e0
Johannes Ekberg <uppfinnarn@gmail.com>
2014-04-19 14:37:18 +0200

    Preparation for internationalization
    Plus some cleanup of the main() function...

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include "KCMainWindow.h"
 #include <QApplication>
+#include <QLibraryInfo>
 #include <QDir>
 #include <QDebug>
+#include "KCMainWindow.h"
 #include "version.h"
 
 int main(int argc, char *argv[])
 {
 	// Make an application!
 	QApplication a(argc, argv);
-
 #ifdef Q_OS_WIN
 	QDir::setCurrent(QCoreApplication::applicationDirPath());
 #endif
@@ -19,17 +19,33 @@ int main(int argc, char *argv[])
 	QCoreApplication::setApplicationVersion(KCT_VERSION);
 	QCoreApplication::setOrganizationName("KanColleTool");
 
-	QStringList args = QCoreApplication::arguments();
+	qDebug() << "Locale:" << QLocale::system().name();
+
+	QTranslator qtTranslator;
+	qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	a.installTranslator(&qtTranslator);
+
+	QTranslator translator;
+	translator.load("kancolletool_" + QLocale::system().name());
+	a.installTranslator(&translator);
 
 	// We want to stay in the tray, not quit when the window goes away
-	if(!args.contains("--exit-on-close"))
-		a.setQuitOnLastWindowClosed(false);
+	a.setQuitOnLastWindowClosed(false);
 
-	// Create the window, which owns the tray icon, but don't show it!
+	// Load commandline arguments
+	QStringList args = QCoreApplication::arguments();
+	{
+		// --exit-on-close = exit when the window is closed; for debugging
+		if(args.contains("--exit-on-close"))
+			a.setQuitOnLastWindowClosed(true);
+	}
+
+	// Create the window
 	KCMainWindow w;
-	if(!w.init()) return 0;
-	w.show(); // Not in the final version!
+	if(!w.init())
+		return 1;
 
-	// Aaand... start the main loop
+	// Aaand... start!
+	w.show();
 	return a.exec();
 }
