a33cd3a8aa1fc1db148b1112fdb327e7612fcaaa
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-23 21:21:59 +0200

    Okay, that was incredibly stupid

--- src/KCFleet.cpp
+++ src/KCFleet.cpp
@@ -4,6 +4,7 @@
 KCFleet::KCFleet(const QVariantMap &data, int loadId, KCClient *parent) :
 	KCGameObject(parent) {
 	connect(this, SIGNAL(missionCompleted()), parent, SLOT(onMissionCompleted()));
+	connect(&missionTimer, SIGNAL(timeout()), this, SLOT(missionCompleted()));
 	missionTimer.setSingleShot(true);
 	loadFrom(data, loadId);
 }
