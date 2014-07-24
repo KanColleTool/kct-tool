e81ba05e9713c59d4d37b098ebb0e60aef944fd4
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-24 14:09:12 +0200

    Updated ehttp

--- lib/includes.cmake
+++ lib/includes.cmake
@@ -1,3 +1,7 @@
+if(MSVC)
+	# No, gmtime() is not an insecure function, fuck you
+	add_definitions(/D_SCL_SECURE_NO_WARNINGS=1)
+endif()
 include_directories(${PROJECT_SOURCE_DIR}/lib/ehttp/include)
 
 add_definitions(-DGROWL_STATIC)
