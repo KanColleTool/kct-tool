5e24986b77cb119a6338b3744d9d306966f604e0
Yukarin <yukariin@yandex.ru>
2015-01-07 00:50:56 +0300

    Remove kclib

--- lib/includes.cmake
+++ lib/includes.cmake
@@ -3,8 +3,6 @@ if(MSVC)
 	add_definitions(/D_SCL_SECURE_NO_WARNINGS=1)
 endif()
 
-include_directories(${PROJECT_SOURCE_DIR}/lib/kclib/src)
-
 include_directories(${PROJECT_SOURCE_DIR}/lib/ehttp/include)
 
 add_definitions(-DGROWL_STATIC)
